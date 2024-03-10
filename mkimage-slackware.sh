94690e16e3e85ca4530db8e57d28c66b773f185e
Andrew Clemons <andrew.clemons@gmail.com>
2024-03-10 13:11:58 +0900

    Install gnupg2 which is required for -current.

--- mkimage-slackware.sh
+++ mkimage-slackware.sh
@@ -99,6 +99,20 @@ if [ "$VERSION" = "15.0" ] && [ "$ARCH" = "arm" ] ; then
 	$base_pkgs"
 fi
 
+# TODO mark as 15.1
+if [ "$VERSION" = "current" ] ; then
+	base_pkgs="$base_pkgs \
+	ap/sqlite \
+	n/libgcrypt \
+	n/libassuan \
+	n/npth \
+	n/libgpg-error \
+	l/icu4c \
+	n/libksba \
+	n/gnupg2"
+fi
+
+
 function cacheit() {
 	local file=$1
 	local check=$2
@@ -288,13 +302,17 @@ cd mnt
 PATH=/bin:/sbin:/usr/bin:/usr/sbin \
 chroot . /bin/sh -c '/sbin/ldconfig'
 
-if [ ! -e ./root/.gnupg ] && [ -e ./usr/bin/gpg ] ; then
+if [ ! -e ./root/.gnupg ] && { [ -e ./usr/bin/gpg ] || [ -e ./usr/bin/gpg2 ] ; } ; then
 	cacheit "GPG-KEY" "md5"
 	cp ${CACHEFS}/GPG-KEY .
+	if [ ! -e ./dev/null ] ; then
+		touch ./dev/null
+	fi
 	echo PATH=/bin:/sbin:/usr/bin:/usr/sbin \
 	GNUPGHOME='' chroot . /usr/bin/gpg --import GPG-KEY
 	PATH=/bin:/sbin:/usr/bin:/usr/sbin \
 	GNUPGHOME='' chroot . /usr/bin/gpg --import GPG-KEY
+	find ./root/.gnupg -name '*.lock' -exec rm -rf {} \;
 	rm GPG-KEY
 fi
 
